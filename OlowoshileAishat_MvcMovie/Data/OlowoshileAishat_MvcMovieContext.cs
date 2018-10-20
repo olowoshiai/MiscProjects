f3b836492b4c4354bccd402ec763e974cca9da74
olowoshiai <olowoshiai@mnstate.edu>
2018-10-20 01:32:46 -0400

    first commit

---
+++ OlowoshileAishat_MvcMovie/Data/OlowoshileAishat_MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OlowoshileAishat_MvcMovie.Models
+{
+    public class OlowoshileAishat_MvcMovieContext : DbContext
+    {
+        public OlowoshileAishat_MvcMovieContext (DbContextOptions<OlowoshileAishat_MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OlowoshileAishat_MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
